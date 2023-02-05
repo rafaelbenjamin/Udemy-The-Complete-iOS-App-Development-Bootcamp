d13d058f35bacd97e4d330d38cabbe344b7ca2f4
Rafael Benjamin <rafaelb.90@hotmail.com>
2023-02-05 13:54:29 -0300

    Including Dicee in SwiftUI project

---
+++ 18.Dicee-SwiftUI/Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Rafael Benjamin on 05/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+                .cornerRadius(4)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let number: Int
+    
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 18.Dicee-SwiftUI/Dicee-SwiftUI/Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Rafael Benjamin on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
