299367cf7c9e87b64808847cf8c671d94af21d9a
Rafael Benjamin <rafaelb.90@hotmail.com>
2023-02-06 20:21:28 -0300

    Including project haxor news

---
+++ 19.H4X0R News-SwiftUI/H4X0R News/H4X0R News/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  H4X0R News
+//
+//  Created by Rafael Benjamin on 06/02/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
---
+++ 19.H4X0R News-SwiftUI/H4X0R News/H4X0R News/Views/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Rafael Benjamin on 06/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination:  DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
